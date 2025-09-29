781014d4c5cfb178fa220763be9e7d9cf8a4b068
alixer-alex <152109823+alixer-alex@users.noreply.github.com>
2025-09-29 17:21:42 -0400

    button now orks

--- frontend/src/Button.jsx
+++ frontend/src/Button.jsx
@@ -4,11 +4,13 @@ function Button({link,name,Image}) {
 
     return( 
             <>
-                <div className = "flex  items-center text-center justify-center text-5xl px-3 py-1 hover:text-gray-500 hover:cursor-pointer gap-2">
+                <a href = {link} target="_blank" className=" flex items-center text-center justify-center text-5xl px-3 py-1 hover:text-gray-500 hover:cursor-pointer gap-2">
                     <Image />
-                    <a href = {link} target="_blank" className="text-sm"> {name}</a>
-
-                </div>
+                    <div className= "text-sm">
+                        {name}
+                    </div>
+                    
+                </a>
             </>
         )
 
